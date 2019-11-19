8b0d013affdab1378582b4e99324a754c7b91ee1
Romain Beauxis <toots@rastageeks.org>
2019-11-18 23:33:15 -0600

    Ffmpeg video decoding/encoding support.

--- src/encoder_formats/ffmpeg_format.ml
+++ src/encoder_formats/ffmpeg_format.ml
@@ -20,38 +20,65 @@
 
  *****************************************************************************)
 
-type opt_val = [
-  | `String of string
-  | `Int of int
-  | `Float of float
-]
+type opt_val = [`String of string | `Int of int | `Float of float]
+
+type output = [`Stream | `Url of string]
 
 type opts = (string, opt_val) Hashtbl.t
 
 type t = {
-  format     : string;
-  codec      : string;
-  channels   : int;
-  samplerate : int Lazy.t ;
-  options    : opts
+  format: string option;
+  output: output;
+  audio_codec: string option;
+  channels: int;
+  samplerate: int Lazy.t;
+  video_codec: string option;
+  options: opts;
 }
 
 let string_of_options options =
   let _v = function
-    | `String s -> Printf.sprintf "%S" s
-    | `Int i -> string_of_int i
-    | `Float f -> string_of_float f
+    | `String s ->
+        Printf.sprintf "%S" s
+    | `Int i ->
+        string_of_int i
+    | `Float f ->
+        string_of_float f
   in
   String.concat ","
-    (Hashtbl.fold (fun k v c ->
-      let v = Printf.sprintf "%s=%s" k (_v v) in
-      v::c) options [])
+    (Hashtbl.fold
+       (fun k v c ->
+         let v = Printf.sprintf "%s=%s" k (_v v) in
+         v :: c)
+       options [])
 
 let to_string m =
-  let opts =
-    string_of_options m.options
+  let format =
+    match m.format with Some f -> Printf.sprintf "format=%S," f | None -> ""
   in
-  Printf.sprintf
-    "%%fmpeg(format=%S,codec=%S,ac=%d,ar=%d%s)"
-    m.format m.codec m.channels (Lazy.force m.samplerate)
+  let opts = string_of_options m.options in
+  let audio_codec =
+    match m.audio_codec with
+      | None ->
+          ""
+      | Some c ->
+          Printf.sprintf ",audio_codec=%S" c
+  in
+  let video_codec =
+    match m.video_codec with
+      | None ->
+          ""
+      | Some c ->
+          Printf.sprintf ",video_codec=%S" c
+  in
+  let output =
+    match m.output with
+      | `Stream ->
+          ""
+      | `Url path ->
+          Printf.sprintf ",url=%S" path
+  in
+  Printf.sprintf "%%fmpeg(%s%s,ac=%d,ar=%d%s%s%s)" format audio_codec
+    m.channels (Lazy.force m.samplerate) video_codec
     (if opts = "" then "" else Printf.sprintf ",%s" opts)
+    output
